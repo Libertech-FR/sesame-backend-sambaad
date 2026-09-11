6be335783c05e23ae953fa4e496e79bd9f57df3c
Alain Abbas <alain.abbas@libertech.fr>
2026-09-11 17:51:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,9 +44,6 @@ echo "host=${HOST}" > ${CONFFILE}
 echo "user=${DN}" >> ${CONFFILE}
 echo "password=${PASSWORD}" >> ${CONFFILE}
 echo "base=${BASE}" >> ${CONFFILE}
-echo "userbase=${USERBASE},${BASE}" >> ${CONFFILE}
-echo "rdnattribute=${RDN}" >> ${CONFFILE}
-echo "backendFor=etd,adm,esn" >> ${CONFFILE}
 chmod 600 ${CONFFILE}
 systemctl restart sesame-daemon
 echo "Vous pouvez completer le fichier de configuration avec les parametres optionnels (voir README.md)"
